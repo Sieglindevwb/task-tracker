08416f1e133e338d051d00bc9c35542d696ab1a4
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-01-30 15:32:28 +0100

    Make login controller instead of usercontroller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']) -> name('home');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login.index');
+
+
