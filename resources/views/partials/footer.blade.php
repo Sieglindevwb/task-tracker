693897a0bfda1519b5d2cbe9645323f3f1e0d7c4
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-03-04 18:43:31 +0100

    Add partials folder with footer and nav

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- Footer -->
+  <footer class="bg-blue-500 text-white py-8">
+    <div class="container mx-auto text-center">
+        <p>&copy; 2024 Planning Plan Planner by Siggy. All rights reserved.</p>
+    </div>
+  </footer>
