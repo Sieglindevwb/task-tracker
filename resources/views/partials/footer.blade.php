d31f99b7ea5e2ac24ebef91061b9bf5fb7c8d3bd
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-03-05 09:18:29 +0100

    Fix year in footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
   <footer class="bg-blue-500 text-white py-8">
     <div class="container mx-auto text-center">
-        <p>&copy; 2024 Planning Plan Planner by Siggy. All rights reserved.</p>
+        <p>&copy; {{now()->year}} Planning Plan Planner by Siggy. All rights reserved.</p>
     </div>
   </footer>
